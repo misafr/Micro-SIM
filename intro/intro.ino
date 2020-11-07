0ceda47dacc4ac1d81122c5226ae4db09bb70fe3
misafr <misafr11@gmail.com>
2020-11-06 20:00:56 -0600

    inicio

---
+++ intro/intro.ino
@@ -0,0 +1,28 @@
+#include "WiFi.h"
+
+// WiFi parameters to be configured
+const char* ssid = "Wifi";
+const char* password = "misael123";
+
+void setup(void)
+{ 
+  Serial.begin(9600);
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+
+  // while wifi not connected yet, print '.'
+  // then after it connected, get out of the loop
+  while (WiFi.status() != WL_CONNECTED) {
+     delay(5000);
+     Serial.print(".");
+  }
+  //print a new line, then print WiFi connected and the IP address
+  Serial.println("");
+  Serial.println("WiFi connected");
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+
+}
+void loop() {
+  // Nothing
+}
